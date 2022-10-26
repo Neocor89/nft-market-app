901d09926f65d3d1bfe1d03fc7673de63ece1a35
Ben <ncor89b@gmail.com>
2022-10-26 14:26:53 +0200

    nft market place finished smart-contract deployed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,88 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-// import Image from 'next/image'
-//: Page center element flex min-h-screen flex-col items-center justify-center py-2
+import type { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { sanityClient, urlFor } from '../sanity';
+import { Collection } from '../typings';
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[];
+}
+
+const Home = ({ collections }: Props) => {
   return (
-    <div className="">
+    <div className="bg-gradient-to-br from-purple-500 to-orange-500 mx-auto flex min-h-screen max-w7xl flex-col py-16 px-6 sm:px-10 2xl:px-0">
       <Head>
         <title>NFT App Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-4xl font-bold text-red-500">Welcome to NFT Drop APP</h1>
+      <h1 className="mb-5 cursor-pointer sm:text-3xl text-md text-[1.rem] font-extralight">
+          The{' '}
+          <span className="font-extrabold underline decoration-white">
+            BENDEVWEB
+          </span>{' '} 
+          NFT Market Place
+        </h1>
 
+        <main className="bg-slate-100 p-6 lg:p-10 shadow-xl shadow-rose-300/20">
+          <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {collections.map(collection => (
+              <Link href={`/nft/${collection.slug.current}`}>
+              <div className="flex flex-col cursor-pointer items-center transition-all duration-200 hover:scale-105">
+                <img 
+                  className="h-50 lg:h-56 w-80 rounded-2xl object-cover mb-2" 
+                  src={urlFor(collection.mainImage).url()} 
+                  alt="BENDEVWEB image collection" />
+
+                <div>
+                  {/* Adding text-center to h1 & h2 for mobile screen devices */}
+                  <h2 className="text-[0.9rem] lg:text-xl font-semibold">{collection.title}</h2>
+                  <p className="mt-2 text-sm text-rose-600">{collection.description}</p>
+                </div>
+              </div>
+              </Link>
+            ))}
+          </div>
+        </main>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"] {
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+    asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+    current
+  },
+  creator-> {
+    _id,
+    name,
+    address,
+    slug {
+    current
+  },
+  },
+  }`
+
+  const collections = await sanityClient.fetch(query);
+  
+
+  return {
+    props: {
+      collections
+    }
+  }
+  
+}
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,14 @@
-// import Image from 'next/image'
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
-function NFTDropPage() {
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { sanityClient, urlFor } from "../../sanity";
+import { Collection } from "../../typings";
+
+interface Props {
+  collection: Collection
+}
+
+function NFTDropPage({collection}: Props) {
 
   //: Auth  
   const connectWithMetamask = useMetamask();
@@ -14,13 +22,18 @@ function NFTDropPage() {
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
           <div className="mt-2 rounded-xl bg-gradient-to-br from-pink-400 to-purple-600 p-2 lg:mt-0">
             <img
-              src="https://res.cloudinary.com/dwoifuutn/image/upload/v1666529141/bored-king_yksqps.png"
-              className="w-32 rounded-xl object-cover lg:h-80 md:w-56 lg:w-72" alt="monkey APE King"
+              src={urlFor(collection.previewImage).url()}
+              className="w-32 rounded-xl object-cover lg:h-80 md:w-56 lg:w-72" 
+              alt="monkey APE King"
             />
           </div>
         <div className=" space-y-2 p-5 text-center">
-          <h1 className="text-2xl lg:text-[2rem] font-bold text-white">BENDEVWEB Apes</h1>
-          <h2 className="text-[1rem] md:text-xl text-gray-300">A collection of BENDEVWEB who live & breathe React</h2>
+          <h1 className="text-2xl lg:text-[2rem] font-bold text-white">
+            {collection.nftCollectionName}
+          </h1>
+          <h2 className="text-[1rem] md:text-xl text-gray-300">
+            {collection.description}
+          </h2>
         </div>
       </div>
       </div>
@@ -28,6 +41,7 @@ function NFTDropPage() {
       <div className="flex flex-1 flex-col lg:p-12 md:p-10 p-6 lg:col-span-6">
       {/*  Header  */}
       <header className="flex items-center justify-between">
+        <Link href={`/`}>
         <h1 className="w-44 cursor-pointer md:text-2xl text-[1.rem] font-extralight sm:w-80">
           The{' '}
           <span className="font-extrabold underline decoration-pink-600/50">
@@ -35,6 +49,8 @@ function NFTDropPage() {
           </span>{' '} 
           NFT Market Place
         </h1>
+        </Link>
+        
         <button onClick={() => (address ? disconnect() : connectWithMetamask())} className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white md:px-7 md:py-2 md:text-base">
           {address ? "Sign Out" : "Sign In"}
         </button>
@@ -45,10 +61,11 @@ function NFTDropPage() {
 
       {/* Main Content  */}
       <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
-      <img className="object-contain pb-10 lg:h-72 w-80" src="https://res.cloudinary.com/dwoifuutn/image/upload/v1666536960/bored-landing-page_jgaklg.png" alt="multiple Bored monkey" />
-      <h1 className=" text-lg font-bold md:text-3xl lg:text-[2.rem] lg:font-extrabold">The BENDEVWEB Ape Coding Club
-      NFT Project</h1>
-      <p className="pt-2 text-[1rem] text-pink-600/90">13 /21 NFT's Claimed</p>
+      <img className="object-contain pb-10 lg:h-72 w-80" src={urlFor(collection.mainImage).url()} alt="multiple Bored monkey" />
+      <h1 className=" text-lg font-bold md:text-3xl lg:text-[2.rem] lg:font-extrabold">{collection.title}</h1>
+      <p className="pt-2 text-[1rem] text-pink-600/90">
+        13 /21 NFT's Claimed
+      </p>
       </div>
 
       <button className="h-12 font-bold md:h-16 w-full rounded-full text-white bg-gradient-to-br from-purple-500 to-orange-500 mt-5 md:mt-10 mb-1 outline-none">Mint NTF (0.01 ETH)</button>
@@ -59,3 +76,46 @@ function NFTDropPage() {
 }
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const query = `*[_type == "collection" && slug.current == $id][0] {
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+    asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+        current
+      },
+    },
+  }`
+
+  const collection = await sanityClient.fetch(query, {
+    id: params?.id,
+  })
+
+  if(!collection) {
+    return {
+      notFound: true,
+    }
+  };
+
+  return {
+    props: {
+      collection
+    }
+  }
+}
